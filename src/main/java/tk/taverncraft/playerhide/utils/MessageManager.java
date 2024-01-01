5abbad44d35b5e1664f64d1265fd343953a14da6
tjtanjin <cjtanjin@gmail.com>
2024-01-01 21:54:10 +0800

    feat: Add support for hex color code and fix permission nodes

--- src/main/java/tk/taverncraft/playerhide/utils/MessageManager.java
+++ src/main/java/tk/taverncraft/playerhide/utils/MessageManager.java
@@ -2,6 +2,8 @@ package tk.taverncraft.playerhide.utils;
 
 import java.util.HashMap;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
@@ -14,88 +16,113 @@ import static org.bukkit.util.ChatPaginator.GUARANTEED_NO_WRAP_CHAT_PAGE_WIDTH;
  * MessageManager handles all formatting and sending of messages to the command sender.
  */
 public class MessageManager {
-  private static final HashMap<String, String> messageKeysMap = new HashMap<>();
+    private static final HashMap<String, String> messageKeysMap = new HashMap<>();
 
-  /**
-   * Sets the messages to use.
-   *
-   * @param lang the configuration to base the messages on
-   */
-  public static void setMessages(FileConfiguration lang) {
-    Set<String> messageKeysSet = lang.getConfigurationSection("").getKeys(false);
+    /**
+     * Sets the messages to use.
+     *
+     * @param lang the configuration to base the messages on
+     */
+    public static void setMessages(FileConfiguration lang) {
+        Set<String> messageKeysSet = lang.getConfigurationSection("").getKeys(false);
 
-    for (String messageKey : messageKeysSet) {
-      messageKeysMap.put(messageKey, ChatColor.translateAlternateColorCodes('&',
-          lang.get(messageKey).toString() + " "));
-    }
-  }
-
-  /**
-   * Sends message to the sender.
-   *
-   * @param sender sender to send message to
-   * @param messageKey key to get message with
-   */
-  public static void sendMessage(CommandSender sender, String messageKey) {
-    String message = getMessage(messageKey);
-    sender.sendMessage(message);
-  }
-
-  /**
-   * Sends message to the sender, replacing placeholders.
-   *
-   * @param sender sender to send message to
-   * @param messageKey key to get message with
-   * @param keys placeholder keys
-   * @param values placeholder values
-   */
-  public static void sendMessage(CommandSender sender, String messageKey, String[] keys, String[] values) {
-    String message = getMessage(messageKey);
-    for (int i = 0; i < keys.length; i++) {
-      message = message.replaceAll(keys[i], values[i]);
-    }
-    sender.sendMessage(message);
-  }
-
-  /**
-   * Retrieves message value given the message key.
-   *
-   * @param messageKey key to retrieve message with
-   */
-  public static String getMessage(String messageKey) {
-    String prefix = messageKeysMap.get("prefix");
-    return prefix.substring(0, prefix.length() - 1) + messageKeysMap.get(messageKey);
-  }
-
-  /**
-   * Shows help menu to the user.
-   *
-   * @param sender sender to send message to
-   * @param pageNum page number to view
-   */
-  public static void showHelpBoard(CommandSender sender, int pageNum) {
-    int linesPerPage = 12;
-    int positionsPerPage = 10;
-
-    String header = getMessage("help-header");
-    String footer = messageKeysMap.get("help-footer");
-    String[] messageBody = messageKeysMap.get("help-body").split("\n", -1);
-    StringBuilder message = new StringBuilder(header + "\n");
-    int position = 1;
-    int currentPage = 1;
-    for (String body : messageBody) {
-      message.append(body).append("\n");
-      if (position % positionsPerPage == 0) {
-        currentPage++;
-        message = new StringBuilder(message.append(footer).append("\n").toString().replaceAll("%page%", String.valueOf(currentPage)));
-        message.append(header).append("\n");
-      }
-      position++;
+        for (String messageKey : messageKeysSet) {
+            messageKeysMap.put(messageKey, formatMessageColor(lang.get(messageKey).toString() + " "));
+        }
     }
 
-    ChatPaginator.ChatPage page = ChatPaginator.paginate(message.toString(), pageNum, GUARANTEED_NO_WRAP_CHAT_PAGE_WIDTH, linesPerPage);
-    for (String line : page.getLines()) {
-      sender.sendMessage(line);
+    /**
+     * Sends message to the sender.
+     *
+     * @param sender sender to send message to
+     * @param messageKey key to get message with
+     */
+    public static void sendMessage(CommandSender sender, String messageKey) {
+        String message = getMessage(messageKey);
+        sender.sendMessage(message);
     }
-  }
+
+    /**
+     * Sends message to the sender, replacing placeholders.
+     *
+     * @param sender sender to send message to
+     * @param messageKey key to get message with
+     * @param keys placeholder keys
+     * @param values placeholder values
+     */
+    public static void sendMessage(CommandSender sender, String messageKey, String[] keys, String[] values) {
+        String message = getMessage(messageKey);
+        for (int i = 0; i < keys.length; i++) {
+            message = message.replaceAll(keys[i], values[i]);
+        }
+        sender.sendMessage(message);
+    }
+
+    /**
+     * Retrieves message value given the message key.
+     *
+     * @param messageKey key to retrieve message with
+     */
+    public static String getMessage(String messageKey) {
+        String prefix = messageKeysMap.get("prefix");
+        return prefix.substring(0, prefix.length() - 1) + messageKeysMap.get(messageKey);
+    }
+
+    /**
+     * Shows help menu to the user.
+     *
+     * @param sender sender to send message to
+     * @param pageNum page number to view
+     */
+    public static void showHelpBoard(CommandSender sender, int pageNum) {
+        int linesPerPage = 12;
+        int positionsPerPage = 10;
+
+        String header = getMessage("help-header");
+        String footer = messageKeysMap.get("help-footer");
+        String[] messageBody = messageKeysMap.get("help-body").split("\n", -1);
+        StringBuilder message = new StringBuilder(header + "\n");
+        int position = 1;
+        int currentPage = 1;
+        for (String body : messageBody) {
+            message.append(body).append("\n");
+            if (position % positionsPerPage == 0) {
+                currentPage++;
+                message = new StringBuilder(message.append(footer).append("\n").toString().replaceAll("%page%", String.valueOf(currentPage)));
+                message.append(header).append("\n");
+            }
+            position++;
+        }
+
+        ChatPaginator.ChatPage page = ChatPaginator.paginate(message.toString(), pageNum, GUARANTEED_NO_WRAP_CHAT_PAGE_WIDTH, linesPerPage);
+        for (String line : page.getLines()) {
+            sender.sendMessage(line);
+        }
+    }
+
+    /**
+     * Formats color in chat messages.
+     *
+     * @param message message to format
+     */
+    private static String formatMessageColor(String message) {
+        Pattern pattern = Pattern.compile("(?<!\\\\)#[a-fA-F0-9]{6}");
+        Matcher matcher = pattern.matcher(message);
+        while (matcher.find()) {
+            String hexCode = message.substring(matcher.start(), matcher.end());
+            String replaceSharp = hexCode.replace('#', 'x');
+
+            char[] ch = replaceSharp.toCharArray();
+            StringBuilder builder = new StringBuilder("");
+            for (char c : ch) {
+                builder.append("&" + c);
+            }
+
+            message = message.substring(0, matcher.start()) + builder.toString() + message.substring(matcher.end());
+            matcher = pattern.matcher(message);
+        }
+
+        message = message.replace("\\#", "#");
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
 }
--- src/main/java/tk/taverncraft/playerhide/utils/UpdateChecker.java
+++ src/main/java/tk/taverncraft/playerhide/utils/UpdateChecker.java
@@ -11,23 +11,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class UpdateChecker {
 
-  private JavaPlugin plugin;
-  private int resourceId;
+    private JavaPlugin plugin;
+    private int resourceId;
 
-  public UpdateChecker(JavaPlugin plugin, int resourceId) {
-    this.plugin = plugin;
-    this.resourceId = resourceId;
-  }
+    public UpdateChecker(JavaPlugin plugin, int resourceId) {
+        this.plugin = plugin;
+        this.resourceId = resourceId;
+    }
 
-  public void getVersion(final Consumer<String> consumer) {
-    Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
-      try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
-        if (scanner.hasNext()) {
-          consumer.accept(scanner.next());
-        }
-      } catch (IOException exception) {
-        this.plugin.getLogger().info("Cannot look for updates: " + exception.getMessage());
-      }
-    });
-  }
+    public void getVersion(final Consumer<String> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
+                if (scanner.hasNext()) {
+                    consumer.accept(scanner.next());
+                }
+            } catch (IOException exception) {
+                this.plugin.getLogger().info("Cannot look for updates: " + exception.getMessage());
+            }
+        });
+    }
 }
